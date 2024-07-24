3ac54e498c48dff5fe627f2e551963c9debd338a
Prabin Panta <pantaprabin30@gmail.com>
2024-07-24 10:47:31 +0545

    chore: Update project dependencies and remove unused files

--- BetterWorld/BetterWorldDbContext.cs
+++ BetterWorld/BetterWorldDbContext.cs
@@ -7,11 +7,7 @@ namespace BetterWorld
 {
     public class BetterWorldDbContext : DbContext
     {
-        public DbSet<Job> Jobs { get; set; }
-        public DbSet<Organization> Organizations { get; set; }
-        public DbSet<Application> Application { get; set; }
-        public DbSet<User> Users { get; set; }
-        public DbSet<re> re { get; set; }
+        public DbSet<Feedback> Organizations { get; set; }
         public string DbPath { get; }
         public BetterWorldDbContext()
         {
@@ -27,71 +23,20 @@ namespace BetterWorld
         {
             options.UseSqlite($"Data Source={DbPath}");
         }
-        public DbSet<BetterWorld.Models.UserProfileModel> UserProfileModel { get; set; } = default!;
-        public DbSet<BetterWorld.Models.UserViewModel> UserViewModel { get; set; } = default!;
-        public DbSet<BetterWorld.Models.review> review { get; set; } = default!;
+        public DbSet<BetterWorld.Models.FeedbackViewModel> FeedbackViewModels { get; set; } = default!;
     }
 
-    public class Job
+    public class Feedback
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public int OrganizationId { get; set; }
-        public Organization Organization { get; set; }
-        public int UserId { get; set; }
-        public User User { get; set; }
-    }
-
-    public class Organization
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Address { get; set; }
-        public int ContactNo { get; set; }
-        public List<Job> Job = new();
-    }
-
-
-
-
-    public class User
-    {
-        public int Id { get; set; }
-        public string UserName { get; set; }
-        public string Password { get; set; }
-        public string AccountType { get; set; }
-        public DateTime CreatedDate { get; set; }
-
-    }
-
-    public class Application
-    {
-        public int Id { get; set; }
-        public string Email { get; set; }
-        public byte[] CV { get; set; }
-        public string CVFileName { get; set; } // Add this line
-        public string CVContentType { get; set; } // Add this line
-        public int JobId { get; set; }
-        public Job Job { get; set; }
-        public int UserId { get; set; }
-        public User User { get; set; }
-    }
-    public class re
-    {
-        public int Id { get; set; }
-        public int? Age { get; set; }
-        public string Skills { get; set; }
-        public string Location { get; set; }
-        public string Email { get; set; }
-        //public byte[] Pic { get; set; }
-        //public string PicFileName { get; set; } 
-        //public string PicContentType { get; set; }
-        public string PhoneNo { get; set; }
-        public string Bio { get; set; }
-        public int OrganizationId { get; set; }
-        public Organization Organization { get; set; }
-        public int UserId { get; set; }
-        public User User { get; set; }
+        public string Program { get; set; }
+        public string Intake { get; set; }
+        public string Level { get; set; }
+        public string Section { get; set; }
+        public string Semester { get; set; }
+        public string Points { get; set; }
+        public string Tutor { get; set; }
+        public string Module { get; set; }
+        public string Misc { get; set; }
     }
 }
---
+++ BetterWorld/Models/FeedbackViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace BetterWorld.Models
+{
+    public class FeedbackViewModel
+    {
+        public int Id { get; set; }
+        public string Program { get; set; }
+        public string Intake { get; set; }
+        public string Level { get; set; }
+        public string Section { get; set; }
+        public string Semester { get; set; }
+        public string Points { get; set; }
+        public string Tutor { get; set; }
+        public string Module { get; set; }
+        public string Misc { get; set; }
+
+    }
+}
