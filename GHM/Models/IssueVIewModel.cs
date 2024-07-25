b329f33e82d965619520b3c5cf82fbc5f8221b2d
Prabin Panta <pantaprabin30@gmail.com>
2024-07-25 09:15:59 +0545

    added some additional features

---
+++ GHM/Models/IssueVIewModel.cs
@@ -0,0 +1,11 @@
+namespace GHM.Models{
+    public class IssueViewModel{
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string RecommendedSolution { get; set; }
+
+    }
+}
